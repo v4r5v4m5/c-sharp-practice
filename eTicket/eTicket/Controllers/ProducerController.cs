88584bf0f2a38d0f688feeded38ba7221f09ca91
Jayavamsi Villuri <64460929+v4r5v4m5@users.noreply.github.com>
2023-08-18 17:33:56 +0530

    18-08-2023 update
    started working on eTicket MVC app.
    Created DbContextFile, Models and started working on Controllers, Views

---
+++ eTicket/eTicket/Controllers/ProducerController.cs
@@ -0,0 +1,21 @@
+﻿using eTicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTicket.Controllers
+{
+    public class ProducerController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducerController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+            return View(allProducers);
+        }
+    }
+}
---
+++ eTicket/eTicket/Program.cs
@@ -0,0 +1,37 @@
+using eTicket.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// dbcontext configuration
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer("Data Source=SAILS-DM347;Initial Catalog=mvc-eticket-app-db;Integrated Security=True;Pooling=False;Encrypt=False"));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// seed database
+/*AppDbInitializer.Seed(app);*/
+
+app.Run();
